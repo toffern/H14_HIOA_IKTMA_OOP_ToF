541b752ecec75208bc73a6b5af1bf960464930ab
toffern <tof@hivolda.no>
2014-09-12 13:46:51 +0200

    Update - veke 2, lagt til litt kommentarar i "Oppgave_6_alder.java"

--- Veke 2/Oppgave6_alder.java
+++ Veke 2/Oppgave6_alder.java
@@ -8,10 +8,13 @@ public class Oppgave6_alder
 	 String navn = JOptionPane.showInputDialog("Oppgi ditt navn.");
 	 int fodselsaar = Integer.parseInt( JOptionPane.showInputDialog("Oppgi ditt fødselsår.") );
 
+	// Lag en instans av type kalender.
 	 Calendar datetime = Calendar.getInstance();
-	 int aaret = datetime.get(Calendar.YEAR);
+	 int aaretViErI = datetime.get(Calendar.YEAR);
+	 	/* Link til andre kalender-felt (mandag, dag, day_of_week, hour_of_day, etc:
+	 		http://docs.oracle.com/javase/1.5.0/docs/api/constant-values.html#java.util.Calendar.PM  		*/
 
-	 String utskrift = "Hei "+navn+", du er: "+(aaret-fodselsaar);
+	 String utskrift = "Hei "+navn+", du er: "+(aaretViErI-fodselsaar);
      JOptionPane.showMessageDialog(null, utskrift);
 
   }
